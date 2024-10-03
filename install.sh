bd168b60dfb069d66f10c741875b8a8326edfa6a
firashacker <firastom0@gmail.com>
2024-10-04 00:12:45 +0300

    cleaned the code

--- install.sh
+++ install.sh
@@ -1,13 +1,88 @@
-#!/bin/zsh
-TARGET="/mnt"
-ZONE="Asia/Jerusalem"
+#!/bin/bash
+##################################
+## your filesystem mount point ###
+##################################
+MOUNTPOINT="/mnt"
+
+#####################
+## Your TimeZone  ###
+#####################
+TIMEZONE="Asia/Jerusalem"
+
+
+###############
+### LOCALES ###
+###############
 LOCALES="en_US.UTF-8 UTF-8"
-User="firas"
-#HostName="Recovery"
-HostName="Arch"
 
-integer FROM=${1}
 
+#####################
+### Your UserName ###
+#####################
+#User="firas"
+
+#####################
+### Your HostName ###
+#####################
+#HostName="Arch"
+
+if [[ "$(whoami)" != "root" ]];then
+  echo "Please run as root"
+  exit
+fi
+
+getHelp(){
+  echo "#########################################################"
+  echo "#########################################################"
+  echo "### Welcome to ArchLinux Hyprland Installation Script ###"
+  echo "### author: firastom0@gmail.com #########################"
+  echo "#########################################################"
+  echo "#########################################################"
+
+
+  echo -e "\e[33mYour Disk must Be Preformated and mounted like so\e[0m"
+  echo -e "\e[31mRoot partition with compatible format mounted to /mnt\e[0m"
+  echo -e "\e[31mEFI partition with fat32/vfat format mounted to /mnt/boot/efi\e[0m"
+  echo -e "\e[32mYou can use gparted to partition and format your disk\e[0m"
+  echo -e "\e[32mThen mount it to mentioned location\e[0m"
+
+
+  echo -e "\nUSAGE:"
+  echo -e "\e[32msudo ./install.sh -U [YOUR USERNAME] -H [YOUR HOSTNAME]\e[0m"
+
+  exit 0
+}
+
+
+
+while getopts ":U:H:T:M:" flag
+do
+    case "${flag}" in
+        U) User=${OPTARG};;
+        H) HostName=${OPTARG};;
+        M) MOUNTPOINT=${OPTARG};;
+        T) TIMEZONE=${OPTARG};;
+        *) getHelp;;
+    esac
+done
+
+if [ -z "${User}" ];then
+  echo -e "\e[31mErr:please specify User Name \"\-U username \"\e[0m "
+  exit -1
+else if [ -z "${HostName}" ];then
+  echo -e "\e[31mErr:please specify Host Name \"\-H hostname \"\e[0m "
+fi
+fi
+
+
+
+
+
+
+
+############################
+### BASE system packages ###
+############################
 
 
 PACKAGES_BASE=(
@@ -15,7 +90,6 @@ alsa-utils
 bc 
 bluez 
 bluez-utils 
-#btrfs-progs 
 cifs-utils 
 efibootmgr 
 exa 
@@ -24,8 +98,6 @@ ffmpegthumbnailer
 tumbler 
 git 
 grub 
-#grub-theme-vimix 
-#gstreamer 
 gvfs 
 gvfs-afc 
 gvfs-smb 
@@ -57,11 +129,9 @@ linux-firmware-qlogic
 mesa 
 mesa-utils 
 mpd 
-mpc 
 mtools 
 nano 
 nano-syntax-highlighting 
-#ncmpcpp 
 netctl 
 networkmanager 
 neofetch 
@@ -79,7 +149,6 @@ pipewire-pulse
 plymouth 
 polkit-qt5 
 pulsemixer 
-#python-i3ipc 
 p7zip 
 qt5-graphicaleffects 
 ranger 
@@ -94,6 +163,8 @@ usb_modeswitch
 usbutils 
 vulkan-radeon 
 wget 
+wireplumber 
+wireless_tools 
 xdg-utils 
 xdg-user-dirs 
 xf86-input-libinput 
@@ -101,8 +172,6 @@ xf86-video-amdgpu
 x264 
 x265 
 xvidcore 
-wireplumber 
-wireless_tools 
 yad 
 zip 
 zsh 
@@ -110,6 +179,11 @@ zsh-autosuggestions
 zsh-completions 
 zsh-syntax-highlighting 
 )
+
+
+#######################
+### Display Manager ###
+#######################
 
 DISPLAY_MANAGER=(
 sddm 
@@ -122,165 +196,155 @@ wlroots
 )
 
 
+#########################
+### Hyprland Packages ###
+#########################
+
 HYPR=(
 hyprland 
+
+#### hyprland wallpaper engine ####
 hyprpaper 
+
+#### GUI wallpaper setter for wayland ####
 waypaper 
+
+#### hyprland lockscreen daemon ####
 hyprlock 
+
+#### hyprland (sleep/hibernate/suspend) daemon  #####
 hypridle 
+
+#### notification daemons ####
 swaync 
+
+#### apps and other menues launcher ####
 rofi-wayland 
+
+#### desktop portal important for video capture and so ####
 xdg-desktop-portal-gtk 
 xdg-desktop-portal-hyprland 
+
+#### auto start applications ####
 dex 
+#### file manager ####
 thunar 
+thunar-archive-plugin 
+
+#### archive manager ####
+xarchiver 
+
+#### Terminal ####
 kitty 
+
+#### text editor ####
 geany 
 geany-plugins 
+
+#### ScreenShot ####
 grim 
 slurp 
+
+#### Music Player ####
 mpd 
+
+#### video player ####
 mpv 
+
+#### Music Player Controll Panel ####
 ncmpcpp 
+mpc
+
+#### theming apps ####
 qt5ct 
 qt6ct 
 kvantum 
+
+#### clipboard progs
 pastel 
-pavucontrol 
-playerctl 
-waybar 
-swappy 
-#viewnior 
 wl-clipboard 
 cliphist 
-blueberry 
-)
 
-SWAY=(
-alacritty 
-fcft 
-foot 
-geany 
-geany-plugins 
-gobject-introspection 
-grim 
-libdisplay-info 
-libsixel 
-libutf8proc 
-libxpresent 
-luajit 
-mako 
-mpd 
-mpv 
-mujs 
-ncmpcpp 
-network-manager-applet 
-qt5ct 
-qt6ct 
-rofi 
-slurp 
-sndio 
-spdlog 
-sway 
-swaybg 
-swaylock 
-swayidle 
-thunar 
-uchardet 
-upower 
-pastel 
+#### audio settings ####
 pavucontrol 
 playerctl 
-python-i3ipc 
-python-mako 
-python-markdown 
-python-pywal 
-viewnior 
+
+#### status bar ####
 waybar 
-wf-recorder 
-wofi 
-wl-clipboard 
-wlogout 
-wlroots 
-wofi 
-xdg-desktop-portal-wlr 
-#xfce-polkit 
-xorg-xwayland 
-)
 
-BSPWM=(
-alacritty 
-bspwm 
-dex 
-dunst 
-geany 
-geany-plugins 
-i3lock 
-ksuperkey 
-maim 
-mpd 
-mpc 
-ncmpcpp 
-nitrogen 
-pastel 
-pavucontrol  
-picom 
-polybar 
-qt5ct 
-qt6ct 
-rofi 
-rxvt-unicode 
-sxhkd 
-thunar 
-viewnior 
-xfce4-clipman-plugin 
-#xfce-polkit 
-xfce4-power-manager 
-xorg-xsetroot 
-xsettingsd 
-)
-
-APPS=( 
-#alacritty 
-atril 
-blueberry 
-corectrl 
-distrobox 
-firefox 
-gamemode 
-goverlay 
-gparted 
-#kdeconnect 
-#meld 
-mangohud 
-mpv 
-network-manager-applet 
-obs-studio 
-obs-vaapi 
-podman 
-steam 
-thunar 
-thunar-archive-plugin 
-timeshift 
+#### image viewer ####
+#swappy 
 #viewnior 
-xarchiver 
-#xed 
-#xfce4-terminal 
-yad 
-openrazer-daemon 
-openrazer-driver-dkms 
-easyeffects 
-#vim 
-neovim 
-#############
-### THEMES ##
-#############
+nsxiv
+
+#### bluetooth control panel ####
+blueberry 
+
+#### THEME ####
 arc-gtk-theme 
 )
 
+
+#################
+### User Apps ###
+#################
+
+APPS=( 
+#### pdf viewer ####
+atril
+
+#### amd gpu control panel ####
+corectrl 
+
+#### distrobox ####
+distrobox 
+podman 
+
+#### webbrowser ####
+firefox 
+
+#### gaming stuff ####
+gamemode 
+goverlay 
+mangohud 
+steam
+obs-studio 
+obs-vaapi 
+
+#### disk manager ####
+gparted 
+
+#### KDE connect ####
+#kdeconnect 
+
+#### status bar applet for network manager ####
+network-manager-applet 
+
+#### timeshift ####
+timeshift
+
+#### shell popups creator ####
+yad 
+
+#### razer stuff ####
+openrazer-daemon 
+openrazer-driver-dkms 
+
+#### audio mixer ####
+easyeffects 
+
+#### neovim ####
+neovim 
+)
+
+###########################
+### My Development apps ###
+###########################
+
 DEVPACK=(
+neovim
 zed 
-ollama 
+#ollama 
 node 
 npm 
 docker 
@@ -288,6 +352,11 @@ docker-compose
 docker-desktop 
 )
 
+
+
+#############
+### Fonts ###
+#############
 
 FONTS=(
 ttf-iosevka-nerd 
@@ -303,113 +372,135 @@ ttf-nerd-fonts-symbols-common
 ttf-nerd-fonts-symbols-mono 
 )
 
-GNOME=(
-gdm 
-adwaita-cursors 
-adwaita-icon-theme 
-gjs 
-gnome-autoar 
-gnome-backgrounds 
-gnome-bluetooth 
-gnome-browser-connector 
-gnome-characters 
-gnome-color-manager 
-gnome-connections 
-gnome-console 
-gnome-control-center 
-gnome-desktop 
-gnome-desktop-4 
-gnome-desktop-common 
-gnome-keyring 
-gnome-session 
-gnome-settings-daemon 
-gnome-shell 
-gnome-shell-extensions 
-gnome-software 
-gnome-tweaks 
-gthumb 
-gtksourceview3 
-gtksourceview4 
-gvfs 
-gvfs-afc 
-gvfs-smb 
-libadwaita 
-libgnomekbd 
-libsoup 
-libsoup3 
-mutter 
-xdg-desktop-portal-gnome 
-xdg-user-dirs-gtk 
-)
 
-KDE=(
-sddm 
-sddm-kcm 
-plasma-desktop 
-packagekit-qt5 
-krunner 
-kde-gtk-config 
-spectacle 
-kinit 
-kwrite 
-kvantum 
-ark 
-dolphin 
-konsole 
-bluedevil 
-xsettingsd 
-gwenview 
-okular 
-)
 
-integer COUNTER=0
-run(){
-echo "${1} :: ${COUNTER}"
-if (( ${COUNTER} >= ${FROM} ));then
-zsh -c "${1}"
-if (( $? != 0 ));then
-echo "Error: ${1}"
-echo ${COUNTER}
-exit
-fi
-fi
-COUNTER=${COUNTER}+1
+
+
+#### sync timezone before installation ####
+
+preInstall(){
+  hwclock --systohc
+  timedatectl set-ntp yes
+  #sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+  sleep 2
 }
 
-#  ${PACKAGES_BASE}  ${DISPLAY_MANAGER}  ${SWAY} ${BSPWM} ${APPS} ${FONTS} ${KDE} ${GNOME} ${HYPR}
-hwclock --systohc
-timedatectl set-ntp yes
-#sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-sleep 2
+
+checkErr(){
+  if (( $? == 0 ));then
+    return 1
+  fi 
+  return 0
+}
+
+ensureSuccess(){
+  Command=$@
+  declare -i Counter=3
+  bash -c "${Command}"
+
+  while  $(checkErr) && (( Counter > 0 ));do
+      Counter=( $Counter-1 )
+      echo -e "\e[31m Err: Retrying ... \e[0m"
+      echo -e "\e[31m ${Command}\e[0m"
+      sleep 2
+      bash -c "$Command"
+  done
+
+  if  $(checkErr) ;then
+    echo -e "\e[31mCritical ! \e[0m"
+    echo -e "\e[31mErr: Couldn't succeed running the following Command:\e[0m"
+    echo -e "\e[33m ${Command}\e[0m"
+    exit -2
+  fi
+
+}
 
 
+installLinux(){
+  echo -e "\e[32mInstalling Linux Base ! ... \e[0m"
+  ensuresuccess pacstrap -k ${MOUNTPOINT} base linux linux-headers linux-firmware grub efibootmgr
+  genfstab -u ${MOUNTPOINT} > ${MOUNTPOINT}/etc/fstab
+  rm -r ${MOUNTPOINT}/etc/localtime
+  ensuresuccess ln -sf /usr/share/zoneinfo/${timezone} ${MOUNTPOINT}/etc/localtime
+  arch-chroot ${MOUNTPOINT} hwclock --systohc
+  arch-chroot ${MOUNTPOINT} passwd
+}
+
+installapps(){
+  echo -e "\e[32minstalling desktop and apps ! ... \e[0m"
+  ensuresuccess pacstrap -k ${MOUNTPOINT}  ${packages_base}  ${display_manager} ${hypr} ${apps} ${fonts}
+  ensuresuccess arch-chroot ${MOUNTPOINT} pacman-key --init
+  ensuresuccess arch-chroot ${MOUNTPOINT} pacman-key --populate
+
+}
+
+installlocal(){
+  echo -e "\e[32mInstalling local packages ! ... \e[0m"
+  ensureSuccess pacstrap -KU ${MOUNTPOINT} $(pwd)/packages/*
+}
+
+installAll(){
+  installLinux
+  installApps
+  installLocal
+}
+
+copyOverlay(){
+  echo -e "\e[32mCopying configs over ! ... \e[0m"
+  cp -r overlay/* ${MOUNTPOINT}
+}
+
+setLocales(){
+  echo -e "\e[32mConfiguring locales ! ... \e[0m"
+  sed -i "s/#\(en_US\.UTF-8\)/\1/" ${MOUNTPOINT}/etc/locale.gen
+  arch-chroot ${MOUNTPOINT} locale-gen
+}
+
+setKernelModules(){
+  echo -e "\e[32mAdding kernel modules ! ... \e[0m"
+  Modules=(amdgpu radeon)
+  sed -i "s/MODULES=()/MODULES=(${Modules})/g" /mnt/etc/mkinitcpio.conf
+  sed -i "s/ udev/ udev plymouth/g" /mnt/etc/mkinitcpio.conf
+  sed -i "s/plymouth plymouth/plymouth/g" /mnt/etc/mkinitcpio.conf  
+}
+
+setHostName(){
+  echo -e "\e[32mSetting Host Name ! ... \e[0m"
+  echo ${HostName} > ${MOUNTPOINT}/etc/hostname
+}
+
+setGrub(){
+  echo -e "\e[32mSetting grub boot loader ! ... \e[0m"
+  ensureSuccess arch-chroot ${MOUNTPOINT} grub-install
+  ensureSuccess arch-chroot ${MOUNTPOINT} mkinitcpio -P
+  ensureSuccess arch-chroot ${MOUNTPOINT} grub-mkconfig -o /boot/grub/grub.cfg
+
+  echo -e "\e[32mSetting systemd services ! ... \e[0m"
+  arch-chroot ${MOUNTPOINT} systemctl enable sddm.service
+  arch-chroot ${MOUNTPOINT} systemctl enable NetworkManager.service
+  arch-chroot ${MOUNTPOINT} systemctl enable bluetooth.service
+}
+
+setUser(){
+  echo -e "\e[32mAdding your USER ! ... \e[0m"
+  arch-chroot ${MOUNTPOINT} useradd -m -G wheel -s /bin/zsh ${User} --home /home/${User}
+  sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' ${MOUNTPOINT}/etc/sudoers
+  arch-chroot ${MOUNTPOINT} passwd ${User}
+}
+
+runPostInstall(){
+  arch-chroot ${MOUNTPOINT} sudo -u ${User} post_install_script
+}
 
 
-run "pacstrap -K ${TARGET} base linux linux-headers linux-firmware grub efibootmgr"
-run "genfstab -U ${TARGET} > ${TARGET}/etc/fstab"
-run "ln -s /usr/share/zoneinfo/${ZONE} ${TARGET}/etc/localtime"
-run "arch-chroot ${TARGET} hwclock --systohc"
-run "arch-chroot ${TARGET} passwd"
-run "pacstrap -K ${TARGET}  ${PACKAGES_BASE}  ${DISPLAY_MANAGER} ${HYPR} ${APPS} ${FONTS}"
-run "arch-chroot ${TARGET} pacman-key --init"
-run "arch-chroot ${TARGET} pacman-key --populate"
-#for i in $(pwd)/packages/*;do
-#run "pacstrap -KU ${TARGET} $i"
-#done
-run "pacstrap -KU ${TARGET} $(pwd)/packages/*"
-run "cp -r overlay/* ${TARGET}"
-sed -i "s/#\(en_US\.UTF-8\)/\1/" ${TARGET}/etc/locale.gen
-sed -i "s/MODULES=()/MODULES=(amdgpu radeon)/g" /mnt/etc/mkinitcpio.conf
-sed -i "s/ udev/ udev plymouth/g" /mnt/etc/mkinitcpio.conf
-run "arch-chroot ${TARGET} locale-gen"
-run "echo ${HostName} > ${TARGET}/etc/hostname"
-run "arch-chroot ${TARGET} grub-install"
-run "arch-chroot ${TARGET} mkinitcpio -P"
-run "arch-chroot ${TARGET} grub-mkconfig -o /boot/grub/grub.cfg"
-run "arch-chroot ${TARGET} systemctl enable sddm.service"
-run "arch-chroot ${TARGET} systemctl enable NetworkManager.service"
-run "arch-chroot ${TARGET} systemctl enable bluetooth.service"
-run "arch-chroot ${TARGET} useradd -m -G wheel -s /bin/zsh ${User} --home /home/${User}"
-sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' ${TARGET}/etc/sudoers
-run "arch-chroot ${TARGET} passwd ${User}"
-run "arch-chroot ${TARGET} sudo -u ${User} post_install_script"
+# preInstall installAll copyOverlay setLocales setKernelModules setHostName setGrub setUser runPostInstall
+
+preInstall
+installAll
+copyOverlay
+setLocales
+setKernelModules
+setHostName
+setGrub
+setUser
+runPostInstall
